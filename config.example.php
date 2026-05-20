0f7ba4fec7956bba01094052714197e59bafa3ba
Assx142 <oskar120408@gmail.com>
2026-05-20 15:57:33 +0700

    Create config.example.php

---
+++ config.example.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'online_booking_db');
+define('DB_USER', 'root');
+define('DB_PASS', 'your_password_here');
+define('DB_CHARSET', 'utf8mb4');
